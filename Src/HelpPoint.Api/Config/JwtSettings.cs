04009620ea87de25fce6cd98e20915051163869c
Leonardo Rivas <84354110+lrivas3@users.noreply.github.com>
2025-03-05 19:46:49 -0600

    :fire: delete files from former project structure

--- Src/HelpPoint.Api/Config/JwtSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HelpPoint.Core.Common;
-
-public class JwtSettings
-{
-   public required string Issuer { get; set; }
-   public required string Audience { get; set; }
-   public required string SecretKey { get; set; }
-}
