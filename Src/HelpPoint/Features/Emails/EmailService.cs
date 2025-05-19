6cb74c07e1416674fd2c9da864633b3f39a28629
Leonardo Rivas <84354110+lrivas3@users.noreply.github.com>
2025-05-19 17:52:06 -0600

    :bug: ya no se usa image for header

--- Src/HelpPoint/Features/Emails/EmailService.cs
+++ Src/HelpPoint/Features/Emails/EmailService.cs
@@ -27,32 +27,6 @@ public class EmailService(IOptions<EmailSettings> settings) : IEmailService
 
         var builder = new BodyBuilder { HtmlBody = request.HtmlBody };
 
-        // embed headerImage.png as <img src="cid:headerImage">
-        var headerImagePath = Path.Combine("Templates", "headerImage.png");
-        if (!File.Exists(headerImagePath))
-        {
-            throw new FileNotFoundException($"Email no enviado. No se encontró '{headerImagePath}'");
-        }
-
-        if (builder.LinkedResources != null)
-        {
-            var headerResource = builder.LinkedResources.Add(headerImagePath);
-            headerResource.ContentId = "headerImage";
-        }
-
-        // embed logoImage.png as <img src="cid:logoImage">
-        var logoImagePath = Path.Combine("Templates", "logoImage.png");
-        if (!File.Exists(logoImagePath))
-        {
-            throw new FileNotFoundException($"Email no enviado. No se encontró '{logoImagePath}'");
-        }
-
-        if (builder.LinkedResources != null)
-        {
-            var logoResource = builder.LinkedResources.Add(logoImagePath);
-            logoResource.ContentId = "logoImage";
-        }
-
         email.Body = builder.ToMessageBody();
 
         using var smtp = new SmtpClient();
